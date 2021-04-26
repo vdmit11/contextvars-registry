98df8ec5ecd82ba8e56213822d11c24c69eca51c
Dmitry Vasilyanov <vdmit11@gmail.com>
2021-04-26 20:10:36 +0300

    add more tests for the inject.py module
    Now the tests cover all 5 kinds of sources that I planned:
    
     1. ContextVar()
     2. ContextVarDescriptor()
     3. vars stored in a ContextVarRegistry()
     4. attributes of an arbitrary object
     5. arbitrary getter function that provides the value

--- tests/test_inject.py
+++ tests/test_inject.py
@@ -2,6 +2,7 @@ from contextvars import ContextVar
 
 import pytest
 
+from contextvars_extras.descriptor import ContextVarDescriptor
 from contextvars_extras.inject import inject_context_vars
 from contextvars_extras.registry import ContextVarsRegistry
 
@@ -9,6 +10,28 @@ from contextvars_extras.registry import ContextVarsRegistry
 def test__inject_from_context_var_objects():
     timezone_var = ContextVar("my_project.namespace.timezone")
     locale_var = ContextVar("my_project.namespace.locale")
+
+    @inject_context_vars(timezone_var, locale_var)
+    def _get_values(locale, timezone="UTC"):
+        return (locale, timezone)
+
+    # pylint: disable=no-value-for-parameter
+    with pytest.raises(TypeError):
+        _get_values()
+
+    locale_var.set("en")
+
+    assert _get_values() == ("en", "UTC")
+
+    assert _get_values(locale="en_GB") == ("en_GB", "UTC")
+    assert _get_values(timezone="GMT") == ("en", "GMT")
+    assert _get_values("en_GB", "GMT") == ("en_GB", "GMT")
+
+
+# same as the test above, but with use of ContextVarDescriptor (instead of ContextVar) objects
+def test__inject_from_context_var_descriptors():
+    timezone_var = ContextVarDescriptor("my_project.namespace.timezone")
+    locale_var = ContextVarDescriptor("my_project.namespace.locale")
 
     @inject_context_vars(timezone_var, locale_var)
     def _get_values(locale, timezone="UTC"):
@@ -59,3 +82,50 @@ def test__inject_vars_from_registry():
         with current(timezone="GMT", user_id=1):
             with current(timezone="Antarctica/Troll", user_id=None):
                 assert _get_values() == ("en_GB", "Antarctica/Troll", None)
+
+
+def test__inject_from_arbitrary_object_attributes():
+    class SomeObject:
+        locale = "en"
+
+    some_object = SomeObject()
+
+    @inject_context_vars(some_object)
+    def _get_values(locale=None, timezone=None):
+        return (locale, timezone)
+
+    assert _get_values() == ("en", None)
+    assert _get_values(timezone="UTC") == ("en", "UTC")
+
+    # pylint: disable=attribute-defined-outside-init
+    some_object.locale = "en_GB"
+    assert _get_values() == ("en_GB", None)
+
+    some_object.timezone = "GMT"
+    assert _get_values() == ("en_GB", "GMT")
+
+
+def test__inject_from_getter_function():
+    storage = dict()
+
+    def _get_current_locale(default):
+        return storage.get("locale", default)
+
+    def _get_current_timezone(default):
+        return storage.get("timezone", default)
+
+    @inject_context_vars(locale=_get_current_locale, timezone=_get_current_timezone)
+    def _get_values(locale=None, timezone=None):
+        return locale, timezone
+
+    assert _get_values() == (None, None)
+    assert _get_values(timezone="UTC") == (None, "UTC")
+
+    storage["timezone"] = "GMT"
+    assert _get_values() == (None, "GMT")
+
+    storage["locale"] = "en_GB"
+    assert _get_values() == ("en_GB", "GMT")
+
+    assert _get_values("en", "UTC") == ("en", "UTC")
+    assert _get_values(locale="en", timezone="UTC") == ("en", "UTC")
