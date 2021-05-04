f639eb8d720f2c9e1de1605ec2bbd3db1f78e23c
Dmitry Vasilyanov <vdmit11@gmail.com>
2021-05-04 14:22:02 +0300

    move docstring from class to module
    No changes in the text, just move the docstring to the module.
    
    I have to do this because the docstring bloats, and I'm going to split
    it to sub-sections, and Sphinx doesn't support sub-sections in class
    docstrings.
    
    So, in order to have sections in the docstring, I have to move the
    text form the class to the module docstring.

--- contextvars_extras/registry.py
+++ contextvars_extras/registry.py
@@ -1,3 +1,62 @@
+"""ContextVarsRegistry - a nice ``@property``-like way to access context variables.
+
+This module contains only 1 main class: :class:`ContextVarsRegistry`.
+
+The idea is simple: you create a sub-class, and declare your variables using type annotations:
+
+    >>> class CurrentVars(ContextVarsRegistry):
+    ...    locale: str = 'en_GB'
+    ...    timezone: str = 'Europe/London'
+    ...    user_id: int = None
+    ...    db_session: object
+
+    >>> current = CurrentVars()
+
+When you create a sub-class, all type-hinted members become ContextVar() objects,
+and you can work with them by just getting/setting instance attributes:
+
+    >>> current.locale
+    'en_GB'
+
+    >>> current.timezone
+    'Europe/London'
+
+    >>> current.timezone = 'UTC'
+    >>> current.timezone
+    'UTC'
+
+Getting/setting attributes is automatically mapped to ContextVar.get()/ContextVar.set() calls.
+
+The underlying ContextVar() objects can be managed via class attributes:
+
+    >>> CurrentVars.timezone.get()
+    'UTC'
+
+    >>> token = CurrentVars.timezone.set('GMT')
+    >>> current.timezone
+    'GMT'
+    >>> CurrentVars.timezone.reset(token)
+    >>> current.timezone
+    'UTC'
+
+Well, actually, the above is a little lie: the class members are actially instances of
+ContextVarDescriptor (not ContextVar). It has all the same get()/set()/reset() methods, but it
+is not a subclass (just because ContextVar can't be subclassed, this is a technical limitation).
+
+So class members are ContextVarDescriptor objects:
+
+    >>> CurrentVars.timezone
+    <ContextVarDescriptor name='contextvars_extras.registry.CurrentVars.timezone'>
+
+and its underlying ContextVar can be reached via the `.context_var` attribute:
+
+    >>> CurrentVars.timezone.context_var
+    <ContextVar name='contextvars_extras.registry.CurrentVars.timezone'...>
+
+But in practice, you normally shouldn't need that.
+ContextVarDescriptor should implement all same attributes and methods as ContextVar,
+and thus it can be used instead of ContextVar() object in all cases except isinstance() checks.
+"""
 from __future__ import annotations
 
 import threading
@@ -11,63 +70,7 @@ from contextvars_extras.util import ExceptionDocstringMixin, Missing
 
 
 class ContextVarsRegistry(MutableMapping):
-    """A collection of ContextVar() objects, with nice @property-like way to access them.
-
-    The idea is simple: you create a sub-class, and declare your variables using type annotations:
-
-        >>> class CurrentVars(ContextVarsRegistry):
-        ...    locale: str = 'en_GB'
-        ...    timezone: str = 'Europe/London'
-        ...    user_id: int = None
-        ...    db_session: object
-
-        >>> current = CurrentVars()
-
-    When you create a sub-class, all type-hinted members become ContextVar() objects,
-    and you can work with them by just getting/setting instance attributes:
-
-        >>> current.locale
-        'en_GB'
-
-        >>> current.timezone
-        'Europe/London'
-
-        >>> current.timezone = 'UTC'
-        >>> current.timezone
-        'UTC'
-
-    Getting/setting attributes is automatically mapped to ContextVar.get()/ContextVar.set() calls.
-
-    The underlying ContextVar() objects can be managed via class attributes:
-
-        >>> CurrentVars.timezone.get()
-        'UTC'
-
-        >>> token = CurrentVars.timezone.set('GMT')
-        >>> current.timezone
-        'GMT'
-        >>> CurrentVars.timezone.reset(token)
-        >>> current.timezone
-        'UTC'
-
-    Well, actually, the above is a little lie: the class members are actially instances of
-    ContextVarDescriptor (not ContextVar). It has all the same get()/set()/reset() methods, but it
-    is not a subclass (just because ContextVar can't be subclassed, this is a technical limitation).
-
-    So class members are ContextVarDescriptor objects:
-
-        >>> CurrentVars.timezone
-        <ContextVarDescriptor name='contextvars_extras.registry.CurrentVars.timezone'>
-
-    and its underlying ContextVar can be reached via the `.context_var` attribute:
-
-        >>> CurrentVars.timezone.context_var
-        <ContextVar name='contextvars_extras.registry.CurrentVars.timezone'...>
-
-    But in practice, you normally shouldn't need that.
-    ContextVarDescriptor should implement all same attributes and methods as ContextVar,
-    and thus it can be used instead of ContextVar() object in all cases except isinstance() checks.
-    """
+    """A collection of ContextVar() objects, with nice @property-like way to access them."""
 
     _var_init_on_setattr: bool = True
     """Automatically initialize missing ContextVar() objects when setting attributes?
